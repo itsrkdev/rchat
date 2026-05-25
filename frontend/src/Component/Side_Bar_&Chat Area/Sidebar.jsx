d2c83fd44c2bce12b00a49c23fa65eced4096fde
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-05-25 20:39:44 +0530

    Update Sidebar.jsx

--- frontend/src/Component/Side_Bar_&Chat Area/Sidebar.jsx
+++ frontend/src/Component/Side_Bar_&Chat Area/Sidebar.jsx
@@ -966,6 +966,7 @@ useEffect(() => {
                         ref={remoteVideoRef}
                         autoPlay
                         playsInline
+                        style={{ width: "100%", height: "300px", backgroundColor: "black", border: "2px solid red" }}
                         className="remote-vid"
                     />
 
