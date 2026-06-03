e249713434d772e80b02a865a6ebff2265ec3448
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-06-03 18:12:13 +0530

    Update cloudinaryConfig.js

--- backend/Middleware/cloudinaryConfig.js
+++ backend/Middleware/cloudinaryConfig.js
@@ -32,37 +32,7 @@ const chatStorage = new CloudinaryStorage({
 
 const uploadChat = multer({ storage: chatStorage }); // Chat ke liye naya multer instance
 
-
-// Sab export kar lijiye (upload aur cloudinary pehle bhi ho rahe the, bas uploadChat extra bhej rhe hain)
 module.exports = { upload, uploadChat, cloudinary };
 
 
 
-
-
-
-
-
-// const cloudinary = require('cloudinary').v2;
-// const { CloudinaryStorage } = require('multer-storage-cloudinary');
-// const multer = require('multer');
-
-// // Apne Cloudinary Dashboard se credentials yahan daalein
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY,    
-//   api_secret: process.env.CLOUDINARY_API_SECRET   
-// });
-
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     folder: 'chat_app_dps', // Cloudinary par folder ka naam
-//     allowed_formats: ['jpg', 'png', 'jpeg'],
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = { upload, cloudinary };
